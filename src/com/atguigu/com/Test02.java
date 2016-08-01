7f3dc4b16b8e84d85309a79b8148fa75c8c6eb19
yiming <15511854165@163.com>
2016-08-02 01:14:53 +0800

    222

--- src/com/atguigu/com/Test02.java
+++ src/com/atguigu/com/Test02.java
@@ -4,5 +4,7 @@ public class Test02 {
 	public static void main(String[] args) {
 		System.out.println("........呵呵");
 		System.out.println("xxxxxxx");
+		System.out.println("2333333");
+		System.out.println("44444444");
 	}
 }
